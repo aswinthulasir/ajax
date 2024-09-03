37d544c2d579772ab1ae6147d7e000be12e1b05e
Aswin Thulasi R <125785982+Aswinrthulasi@users.noreply.github.com>
2024-09-03 12:31:43 +0530

    big commit

--- strings.js
+++ strings.js
@@ -4,3 +4,28 @@ console.log(str.indexOf('is'));
 console.log(str.replace('string','Killadi'));
 console.log(str);
 console.log(str.split(' '));
+
+//Array operator
+
+// var arr=[12,14,27,75];
+// arr.forEach(function(item,ind,arr2){
+//     arr2[ind]=item>50;
+// })
+
+// console.log(arr);
+
+var arr=[12,14,27,75];
+// arr.forEach(function (item,ind,arr2){
+//     console.log(item);
+// })
+
+
+//Filter function
+
+var arr2=arr.filter(function(item){
+    return item>50;
+})
+console.log(arr2);
+console.log(arr);
+
+// Checkout map function
